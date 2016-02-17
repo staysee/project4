3d41bf1d0d9757b05fa417ecd49ad3efafad9f28
Stacey Sugiono <staceysugiono@gmail.com>
2016-02-17 03:49:04 -0800

    added background to gallery, styled nav bar for now

--- client/js/app.js
+++ client/js/app.js
@@ -6,13 +6,15 @@ function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: "/",
-      templateUrl: "templates/home.html"
+      templateUrl: "templates/home.html",
+      data: {css: ["assets/css/main.css"]}
     })
     .state('gallery', {
       url: "/gallery",
       templateUrl: "templates/gallery.html",
       controller: "GalleryController",
-      controllerAs: "vm"
+      controllerAs: "vm",
+      data: {css: ["assets/css/gallery.css"]}
     })
     .state('hiring', {
       url: "/hiring",
