0c56ee82d6513f0bdec50a828a053c6b4501739b
Stacey Sugiono <staceysugiono@gmail.com>
2016-02-18 22:03:01 -0800

    have api with token authentication

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,12 +2,48 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+var superSecret = 'thissecretissuperdupersecret'
 var usersController = require('../controllers/userscontroller');
 var postsController = require('../controllers/postscontroller');
 
 
 // router.route('/')
 //   .get(usersController.welcome)
+
+//route middleware to verify a token
+router.use(function (req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  //decode token
+  if (token) {
+
+    //verifies secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded) {
+      if (err) {
+        return res.status(403).send({
+          success: false,
+          message: 'Failed to authenticate token.'
+        })
+      } else {
+        //if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    })
+  } else {
+    //if there's no token return HTTP response 403(access forbidden) and error msg
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    })
+  }
+})
+
+//api endpoint to get logged in user info
+router.get('/me', function (req, res) {
+  res.send(req.decoded)
+})
 
 router.route('/authenticate')
   .post(usersController.authenticate)
